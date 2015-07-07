f29c01ba7c119902d4f59c438c402ed90f70ca09
柯磊 <kelei0017@gmail.com>
2015-07-07 23:17:55 +0800

    首页->维修保养列表界面支持弹出键盘时调整输入框位置

--- Sunflower/FunctionModule/PropertyService/ViewControllers/PropertyFixVC.m
+++ Sunflower/FunctionModule/PropertyService/ViewControllers/PropertyFixVC.m
@@ -1608,6 +1608,7 @@
 
 #pragma mark - Data
 - (void)_setupObserver {
+    [self.fixTableV handleKeyboard];
     _weak(self);
     [self startObserveObject:self forKeyPath:@"fixIssueList" usingBlock:^(NSObject *target, NSString *keyPath, NSDictionary *change) {
         _strong(self);
